e9bd29cad40313bd5b6c2ed6b946932fde73740f
uchino rin <c0a2403756@edu.teu.ac.jp>
2025-11-18 17:22:00 +0900

    ex10

--- pygame_image.py
+++ pygame_image.py
@@ -12,7 +12,11 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_img_r = pg.transform.flip((bg_img),True,False)
     kk_img = pg.transform.flip(pg.image.load("fig/3.png"),True,False)
+    kk_rect = kk_img.get_rect()
+    kk_rect.center= 300, 200
+
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -21,8 +25,17 @@ def main():
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img_r, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(kk_img, [300,200])
 
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rect.move_ip((0, -1))
+        if key_lst[pg.K_DOWN]:
+            kk_rect.move_ip((0, 1))
+        if key_lst[pg.K_LEFT]:
+            kk_rect.move_ip((-1, 0))
+        if key_lst[pg.K_RIGHT]:
+            kk_rect.move_ip((1, 0))
+        screen.blit(kk_img, kk_rect)
         
         pg.display.update()
         tmr += 1        
