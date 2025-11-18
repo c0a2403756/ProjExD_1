1fda6e7c3b1494d3ee59f7cd205a3a0ec2b2de9b
uchino rin <c0a2403756@edu.teu.ac.jp>
2025-11-18 17:11:15 +0900

    ex9

--- pygame_image.py
+++ pygame_image.py
@@ -17,11 +17,13 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr
+        x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img_r, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
         screen.blit(kk_img, [300,200])
 
+        
         pg.display.update()
         tmr += 1        
         clock.tick(200)
